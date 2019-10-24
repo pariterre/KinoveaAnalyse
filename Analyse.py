7c6073eff49197b6091c56776ecd2978bf0d4a1e
Pariterre <pariterre@hotmail.com>
2019-10-24 15:58:51 -0400

    Added CoM on the graph

--- Analyse.py
+++ Analyse.py
@@ -47,15 +47,6 @@ com_i_ddot = BiomechanicsComputation.derivative(com_i_dot, time)
 # Compute GRF
 grf = BiomechanicsComputation.compute_grf(com_ddot, masse)
 
-# Kinogramme
-plt.figure("Kinogramme du mouvement")
-plt.ylabel("Axe vertical (m)")
-plt.xlabel("Axe frontal (m)")
-couleurs = np.linspace(0.88, 0, data.shape[2])
-for i in range(data.shape[2]):
-    plt.plot(data[0, stick, i], data[1, stick, i], color=[couleurs[i], couleurs[i], couleurs[i]])
-plt.axis('equal')
-
 # Output
 plt.figure("Analyse biomécanique de Kinovea")
 
@@ -68,31 +59,52 @@ plt.plot(data[0, stick, time_idx], data[1, stick, time_idx], 'r')
 plt.plot(com_i[0, :, time_idx], com_i[1, :, time_idx], 'k.')
 plt.plot(com[0, 0, time_idx], com[1, 0, time_idx], 'k.', markersize=20)
 plt.axis('equal')
+xlim = plt.xlim()
+plt.text(com[0, 0, time_idx] + (xlim[1] - xlim[0])*0.04, com[1, 0, time_idx],
+         f"CoM = [{str(np.round(com[0, 0, time_idx], 2))}; {str(np.round(com[1, 0, time_idx], 2))}]", fontsize=12)
 
 # Show some calculation
+buffer = (time[-1]-time[0])*0.005
+xlim = (time[0]-buffer, time[-1]+buffer)
 plt.subplot(432)
 plt.title("Hauteur du CoM")
 plt.ylabel("Hauteur (m)")
 plt.tick_params(axis='x', which='both', bottom=False, top=False, labelbottom=False)
 plt.plot(time, com[1, 0, :])
+ylim = plt.ylim()
+plt.plot((time[time_idx], time[time_idx]), plt.ylim(), 'r')
+plt.xlim(xlim)
+plt.ylim(ylim)
 
 plt.subplot(435)
 plt.title("Vitesse verticale")
 plt.ylabel("Vitesse (m/s)")
 plt.tick_params(axis='x', which='both', bottom=False, top=False, labelbottom=False)
 plt.plot(time, com_dot[1, 0, :])
+ylim = plt.ylim()
+plt.plot((time[time_idx], time[time_idx]), plt.ylim(), 'r')
+plt.xlim(xlim)
+plt.ylim(ylim)
 
 plt.subplot(438)
 plt.title("Accélération verticale")
 plt.ylabel("Accélération (m/s²)")
 plt.tick_params(axis='x', which='both', bottom=False, top=False, labelbottom=False)
 plt.plot(time, com_ddot[1, 0, :])
+ylim = plt.ylim()
+plt.plot((time[time_idx], time[time_idx]), plt.ylim(), 'r')
+plt.xlim(xlim)
+plt.ylim(ylim)
 
 plt.subplot(4, 3, 11)
 plt.title("GRF")
 plt.ylabel("GRF (N)")
 plt.xlabel("Temps (s)")
 plt.plot(time, grf[1, 0, :])
+ylim = plt.ylim()
+plt.plot((time[time_idx], time[time_idx]), plt.ylim(), 'r')
+plt.xlim(xlim)
+plt.ylim(ylim)
 
 plt.subplot(1, 3, 3)
 plt.title("Angles articulaire au cours du temps")
@@ -100,10 +112,23 @@ plt.ylabel("Angle (°)")
 plt.xlabel("Temps (s)")
 for joint in angles.values():
     plt.plot(time, KinoveaReader.to_degree(joint))
+ylim = plt.ylim()
+plt.plot((time[time_idx], time[time_idx]), plt.ylim(), 'r')
+plt.xlim(xlim)
+plt.ylim(ylim)
 plt.legend(angles.keys())
 
 plt.tight_layout(h_pad=-1, w_pad=-6)
 figManager = plt.get_current_fig_manager()
 figManager.window.showMaximized()
+
+# Kinogramme
+plt.figure("Kinogramme du mouvement")
+plt.ylabel("Axe vertical (m)")
+plt.xlabel("Axe frontal (m)")
+couleurs = np.linspace(0.88, 0, data.shape[2])
+for i in range(data.shape[2]):
+    plt.plot(data[0, stick, i], data[1, stick, i], color=[couleurs[i], couleurs[i], couleurs[i]])
+plt.axis('equal')
+
 plt.show()
-print("Fin du script")
