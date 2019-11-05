fb9d8b9d36460ebdd5429369bf8ba8eda5c1c3c7
pariterre <pariterre@hotmail.com>
2019-11-05 17:05:01 -0500

    Added the hand

--- models/sagittal.py
+++ models/sagittal.py
@@ -1,28 +1,30 @@
 def model():
 	# Les noms de reperes_anato doivent correspondrent à ceux donnés dans Kinovea
-	reperes_anato = ("Hanche", "Genou", "Malleole", "Pied", "Epaule", "Coude", "Main", "Tete")
+	reperes_anato = ("Hanche", "Genou", "Malleole", "Orteil", "Epaule", "Coude", "Poignet", "Doigt", "Tete")
 
 	# Le stick relie les reperes_anato pour dessiner
-	stick = [3, 2, 1, 0, 4, 7, 4, 5, 6]
+	stick = [3, 2, 1, 0, 4, 8, 4, 5, 6, 7]
 
 	# Winter
 	# Definition des articulation 
 	angle_seg = {
 		# Articulation  # dist          # prox      # center
-		"Cheville":     ("Pied",        "Genou",    "Malleole"),
+		"Cheville":     ("Orteil",      "Genou",    "Malleole"),
 		"Genou":        ("Malleole",    "Hanche",   "Genou"),
 		"Hanche":       ("Genou",       "Epaule",   "Hanche"),
 		"Epaule":       ("Hanche",      "Coude",    "Epaule"),
-		"Coude":        ("Epaule",      "Main",     "Coude")
+		"Coude":        ("Epaule",      "Poignet",  "Coude"),
+		"Poignet":      ("Coude",       "Doigt",    "Poignet")
 	}
 	winter_table = {
 		# Membre:   Seg_prox,    Seg_dist,   Masse,  CM_Prox, CM_Dist,  nb_seg
 		"TT":       ("Epaule",   "Hanche",   0.578,  0.66,    0.34,     1),
 		"Bras":     ("Epaule",   "Coude",    0.028,  0.436,   0.564,    2),
-		"AvBras":   ("Coude",    "Main",     0.016,  0.43,    0.57,     2),
+		"AvBras":   ("Coude",    "Poignet",  0.016,  0.43,    0.57,     2),
+		"Main":     ("Poignet",  "Doigt",    0.006,  0.506,   0.494,    2),
 		"Cuisse":   ("Hanche",   "Genou",    0.1,    0.433,   0.567,    2),
 		"Jambe":    ("Genou",    "Malleole", 0.0465, 0.433,   0.567,    2),
-		"Pied":     ("Malleole", "Pied",     0.0145, 0.5,     0.5,      2)
+		"Pied":     ("Malleole", "Orteil",   0.0145, 0.5,     0.5,      2)
 	}
 	
 	return reperes_anato, stick, angle_seg, winter_table
